4ab7e0c5a22414c1c79513205b87d7fa7fe6b873
Daren Zammit <daren@hive.com.mt>
2017-08-23 00:02:52 +0200

    Added post thumbnail

--- components/post/content-single.php
+++ components/post/content-single.php
@@ -10,6 +10,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
+	<?php if (has_post_thumbnail()): ?>
+	<div class="entry-thumbnail">
+		<?php the_post_thumbnail('large');?>
+	</div>
+	<?php endif ?>
+
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->

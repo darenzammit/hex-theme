4eab2f71430c182cdf85334247c5f03972bd68cb
Daren Zammit <daren@hive.com.mt>
2017-08-31 07:02:11 +0200

    moved search content to post dir

---
+++ components/post/content-none.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'hex' ); ?></p>
+<?php get_search_form(); ?>
